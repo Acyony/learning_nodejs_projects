110cb949cd9d6797792c5f1a08d7c4f238a8b16f
Alcione de França Ribeiro <alcioneribeiro@MacBook-Air-de-Alcione.local>
2021-11-21 23:52:11 +0100

    Connecting with database, creating partials with ejs

---
+++ wordpress/database/database.js
@@ -0,0 +1,19 @@
+const Sequelize= require('sequelize');
+
+const config = {
+    db_name: process.env.DB_NAME || "wordpress",
+    db_user: process.env.DB_USER || "root",
+    db_host: process.env.DB_HOST || "localhost",
+    db_pass: process.env.DB_PASS,
+};
+
+// new Sequelize is an object we use to pass the data to connect with the database
+
+const connection = new Sequelize(
+        config.db_name, config.db_user, config.db_pass, {
+        host: config.db_host,
+        dialect: 'mysql'
+    })
+
+module.exports = connection;
+
--- wordpress/index.js
+++ wordpress/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require("./database/database");
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
+
 
 //View engine
 app.set('view engine', 'ejs');
 
+// static
+app.use(express.static('public'));
+
 //Body parser
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
+// Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Connected to database successfully!')
+    }).catch((err) => {
+    console.log(`error connecting database ${err}`);
+})
 
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080!")
