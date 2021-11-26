69f939a5901de63300aaead5c900fd56676c151f
Alcione <alcionefribeiro@gmail.com>
2021-11-26 21:11:08 +0100

    Add pagination sistem

--- wordpress/articles/ArticleController.js
+++ wordpress/articles/ArticleController.js
@@ -96,7 +96,40 @@ router.post('/articles/update', (req, res) => {
         console.log(err);
         res.redirect('/');
     })
-})
+});
+
+
+router.get('/articles/page/:num', (req, res) => {
+    let page = req.params.num;
+    let offset = 0;
+
+    if (isNaN(page) || page === 1) {
+        offset = 0;
+    } else {
+        offset = parseInt(page) * 4;
+    }
+
+    // setting how many articles I want per page => pagination approach
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles => {
+
+        let next;
+        if (offset + 4 >= articles.count) {
+            next = false;
+        } else {
+            next = true;
+        }
+
+        let result = {
+            next: next,
+            articles: articles
+        }
+
+        res.json(result);
+    })
+});
 
 
 module.exports = router;
