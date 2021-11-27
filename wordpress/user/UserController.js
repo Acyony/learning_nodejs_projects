8b4aef08949267aedd69373339458cde79df7430
Alcione <alcionefribeiro@gmail.com>
2021-11-28 00:09:48 +0100

    Implemeting hash to store data into a database; duplicated emails isn't allowed to logIn

--- wordpress/user/UserController.js
+++ wordpress/user/UserController.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const User = require('./User.js');
+const bcrypt = require('bcryptjs');
+
 
 router.get('/admin/users', (req, res) => {
     res.send('Users list');
@@ -14,6 +17,25 @@ router.post('/users/create', (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    res.json({email: email, password: password});
+    User.findOne({
+        where: {email: email}
+    }).then(user => {
+        if (!user) {
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt);
+            // hash => will be saved on the database
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/');
+            }).catch((err) => {
+                res.redirect('/');
+            })
+        } else {
+            res.redirect('/admin/users/create');
+        }
+    })
 })
 module.exports = router;
